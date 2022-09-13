5c55b5e75a15dce766ffd125f1617c74f29adfd8
Gen Vikat <gen@veebisepad.ee>
2022-09-13 19:00:56 +0300

    Added credentials to env file. Cleared application from console logs

--- api/api.js
+++ api/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const API = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
-        'X-RapidAPI-Key': 'c5b3db7c9dmsh67ca037188eb45cp1f5c7cjsnceb24cc348c6',
-        'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+        'X-RapidAPI-Key': import.meta.env.VITE_X_RAPID_API_KEY,
+        'X-RapidAPI-Host': import.meta.env.VITE_X_RAPID_API_HOST
     }
 })
 
