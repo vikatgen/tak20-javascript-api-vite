e9f82ddf5cb5b4aeebd05526030f9b23a68f8845
Gen Vikat <gen@veebisepad.ee>
2022-10-04 18:32:48 +0300

    Remove console logs from main.js & add more spacing to result wrapper with bigger font size

--- main.js
+++ main.js
@@ -26,7 +26,6 @@ try {
 const fetchCountryStatistics = async (country) => {
 	await API.get('/statistics', { params: { country: country }}).then((response) => {
 		const result = response.data?.response[0]
-		console.log(result)
 
 		if (!result) {
 			emptyStateWrapper.classList.remove('hidden');
